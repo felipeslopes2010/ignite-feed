b8bbc42d3ec1368b9612289748f12d04644ffedf
Felipe Santos <felipe.santos@qualyteam.com.br>
2024-11-13 22:36:36 -0300

    Adjust: Pass props to Post and Comment Component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,78 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { useState } from 'react';
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({ author, content, publishedAt }) {
+    const [comments, setComments] = useState([
+        'Post muito bacana, hein?!'
+    ]);
+
+    const [newCommentText, setNewCommentText] = useState("");
+
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: "true",
+    })
+
+    function handleCreateNewComment(e) {
+        e.preventDefault();
+        
+        setComments([...comments, newCommentText]);
+        
+        setNewCommentText("");
+    }
+
+    function handleNewCommentChange(e) {
+        setNewCommentText(e.target.value);
+    }
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://github.com/diego3g.png" />
+                    <Avatar src={author.avatarUrl} />
 
                     <div className={styles.authorInfo}>
-                        <strong>Felipe Kimura</strong>
-                        <span>Web Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title="12 de Novembro de 2024 às 21:42:00" dateTime="2024-11-12 21:42:00">Públicado há 1 hora</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
             
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-                <p><a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto{' '}</a>
-                    <a href="#">#nlw{' '}</a>
-                    <a href="#">#rocketseat</a>
-                </p>
+                {
+                    content.map(line => {
+                        if(line.type === "paragraph") {
+                            return <p>{line.content}</p>
+                        } else if(line.type === "link") {
+                            return <p><a href="#">{line.content}</a></p>
+                        }
+                    })
+                }
             </div>
 
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                  <strong>Deixe seu feedback</strong>
 
-                 <textarea placeholder=" Escreva um comentário..." />
+                 <textarea 
+                    name="comment"
+                    placeholder=" Escreva um comentário..."
+                    value={newCommentText}
+                    onChange={handleNewCommentChange}
+                />
 
                 <footer>
                     <button type="submit">Publicar</button>
@@ -41,9 +80,11 @@ export function Post() {
             </form>
 
             <div className={styles.commentList}>
-                <Comment />
-                <Comment />
-                <Comment />
+                {
+                    comments.map(comment => {
+                        return <Comment content={comment} />
+                    })
+                }
             </div>
         </article>
     )
